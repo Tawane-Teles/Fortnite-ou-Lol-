8d2c937e74d9c2630106d9156face6a4a496ff98
Tawane <tawanesilva.96@hotmail.com>
2020-11-22 13:58:46 -0300

    adicionando nome e subtitulo

--- app/src/main/java/com/tawane/fortniteoulol/MainActivity.kt
+++ app/src/main/java/com/tawane/fortniteoulol/MainActivity.kt
@@ -2,10 +2,38 @@ package com.tawane.fortniteoulol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.tawane.fortniteoulol.adapter.ListaAdapter
+import com.tawane.fortniteoulol.model.ItemList
+
 
 class MainActivity : AppCompatActivity() {
+    var listaAdapter: ListaAdapter? = null
+    var linearLayoutManager: LinearLayoutManager? = null
+    var nameList = ArrayList<ItemList>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val list: RecyclerView = findViewById(R.id.recyclerview)
+
+        getList()
+        listaAdapter = ListaAdapter(nameList, this)
+        linearLayoutManager = LinearLayoutManager(this)
+        list.layoutManager = linearLayoutManager
+        list.adapter = listaAdapter
+        list.adapter
     }
+
+    private fun getList() {
+        nameList.add(ItemList("jinx", R.drawable.img_jynxperso1, "Criminosa, impulsiva e maníaca."))
+        nameList.add(ItemList("Akali", R.drawable.img_akaliperso2, "Perigosa, assassina e sozinha."))
+        nameList.add(ItemList(name = "Miss Fortune", image = R.drawable.img_missperso3, subTitulo = "Impiedosa, vingativa e sedutora"))
+        nameList.add(ItemList(name = "Raven", image = R.drawable.img_ravenperso4, subTitulo = "Exilado, guerreiro e sombrio"))
+        nameList.add(ItemList(name = "Fable", image = R.drawable.img_fableperso5, subTitulo = "Simpática, doce e vulnerável"))
+    }
+
 }
